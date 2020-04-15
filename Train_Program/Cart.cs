9fccb6fde792046e4219d2871f4a4c413905d530
Risen12 <den2033@bk.ru>
2020-04-15 18:47:31 +0300

    first commit

---
+++ Train_Program/Cart.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Train_Program
+{
+    class Cart
+    {
+        public int OrderID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public IList <Product> Contents {get;set;}
+        public bool OrderCompleted { get; set; }
+        public override string ToString()
+        {
+            string products = "";
+            for (int i = 0; i < Contents.Count; i++)
+            {
+                products += "ID товара: " + Contents[i].ProductID + "; Название товара: " + Contents[i].ProductName + "; Количество товара: " + Contents[i].Quantity + "\n";
+            }
+            return @"Номер заказа: " + OrderID + "\n"
+                   + "Имя заказчика: " + Name + "\n"
+                   + "Email заказчика: " + Email + "\n"
+                   + "Имя заказчика: " + Name + "\n" +
+                   products + "Выполнение заказа: " + Convert.ToString(OrderCompleted);
+        }
+    }
+}
---
+++ Train_Program/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.JScript;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Text.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace Train_Program
+{
+
+    class Program
+    {
+        public static Random rand = new Random(Guid.NewGuid().ToByteArray().Sum(x => x));
+        static void Main()
+        {
+            string s = "{" +
+                        "\"OrderID\": 12345," +
+                        "\"Name\": \"Иван Иванов\"," +
+                        "\"Email\": \"ivanov@gmail.com\"," +
+                        "\"Contents\": [" +
+                        " {" +
+                        " \"ProductID\": 34," +
+                        " \"ProductName\": \"Sony\"," +
+                        " \"Quantity\": 1" +
+                        " }," +
+                        " {" +
+                        " \"ProductID\": 56," +
+                        " \"ProductName\": \"Philips\"," +
+                        " \"Quantity\": 3" +
+                        " }" +
+                        "]," +
+                        "\"OrderCompleted\": true" +
+                        "}";
+            Console.WriteLine(Task12(s));
+            Console.WriteLine(Task12D(Task12(s)));
+        }
+        static Cart Task12(string input)
+        {
+            var serializer = new JavaScriptSerializer();
+            var cart = serializer.Deserialize<Cart>(input);
+            return cart;
+        }
+        static string Task12D(Cart cart)
+        {
+            var serializer = new JavaScriptSerializer();
+            string result = serializer.Serialize(cart);
+            return result;
+        }
+    }
+}
+
+
